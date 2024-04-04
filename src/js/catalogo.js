9b0dd0364df34b82dff37bfbb432e212be6cd4ab
GuilhermePain <guizaorocha543@gmail.com>
2024-04-04 14:09:44 -0300

    Alteração nas funcionalidades de modal

--- src/js/catalogo.js
+++ src/js/catalogo.js
@@ -1,37 +1,30 @@
-function maisInformacoes() {
+document.addEventListener('DOMContentLoaded', function() {
     const btnMaisInformacoes = document.querySelectorAll('.btn-mais-informacoes');
     const modalMaisInformacoes = document.querySelector('.section-mais-informacoes');
     const closeModal = document.querySelector('#close-modal');
+    const modalImagem = document.querySelector('.section-mais-informacoes img');
+    const modalNome = document.querySelector('.section-mais-informacoes .name-mais-informacoes');
+    const modalPreco = document.querySelector('.section-mais-informacoes .price-mais-informacoes');
 
-    btnMaisInformacoes.forEach(buttons => {
-        buttons.addEventListener('click', () => {
+    btnMaisInformacoes.forEach(button => {
+        button.addEventListener('click', function() {
+            const itemCard = this.closest('.item-card');
+            const imagem = itemCard.querySelector('img');
+            const nome = itemCard.querySelector('.item-name').textContent;
+            const preco = itemCard.querySelector('.item-price').textContent;
+
+            // Atualiza a imagem, nome e preço no modal
+            modalImagem.src = imagem.src;
+            modalNome.textContent = nome;
+            modalPreco.textContent = preco;
+
+            // Exibe o modal
             modalMaisInformacoes.style.display = 'flex';
-        })
-    })
+        });
+    });
 
-    closeModal.addEventListener('click', () => {
+    closeModal.addEventListener('click', function() {
+        // Esconde o modal ao clicar no botão de fechar
         modalMaisInformacoes.style.display = 'none';
-    })
-
-    function pedirNoWhatsapp() {
-        const data = new Date();
-        const hora = data.getHours();
-        let saudacao;
-
-        if (hora >= 12 && hora < 18) {
-            saudacao = 'boa tarde!';
-        } else if (hora >= 18 ) {
-            saudacao = 'boa noite!';
-        } else {
-            saudacao = 'bom dia!';
-        }
-
-        const linkWhatsapp = document.querySelector('#link-whatsapp');
-        linkWhatsapp.href = `https://api.whatsapp.com/send?phone=5585991805388&text=Olá, ${saudacao} Vim do catálogo.`;
-
-    }
-
-    pedirNoWhatsapp();
-}
-
-maisInformacoes(); 
+    });
+});
