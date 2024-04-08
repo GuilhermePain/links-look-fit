883cdb25863b01075033b5ab65e0d44edbdc172a
GuilhermePain <guizaorocha543@gmail.com>
2024-04-08 09:19:00 -0300

    Alterações no conteúdo do catálogo

--- src/js/catalogo.js
+++ src/js/catalogo.js
@@ -1,4 +1,4 @@
-function maisInformacoes(){
+function maisInformacoes() {
     const btnMaisInformacoes = document.querySelectorAll('.btn-mais-informacoes');
     const modalMaisInformacoes = document.querySelector('.section-mais-informacoes');
     const closeModal = document.querySelector('#close-modal');
@@ -6,21 +6,30 @@ function maisInformacoes(){
     const modalNome = document.querySelector('.section-mais-informacoes .name-mais-informacoes');
     const modalPreco = document.querySelector('.section-mais-informacoes .price-mais-informacoes');
     const modalDesconto = document.querySelector('.section-mais-informacoes .desconto-mais-informacoes');
-    let posicaoDoScroll = 0;
+    let scrollPosition = 0;
 
     btnMaisInformacoes.forEach(button => {
         button.addEventListener('click', function() {
-            posicaoDoScroll = window.pageYOffset;
+            scrollPosition = window.pageYOffset;
             const itemCard = this.closest('.item-card');
             const imagem = itemCard.querySelector('img');
             const nome = itemCard.querySelector('.item-name').textContent;
-            const preco = itemCard.querySelector('.item-price').textContent;
+            const precoTop = itemCard.querySelector('.item-price-top');
+            const precoShort = itemCard.querySelector('.item-price-short');
             const desconto = itemCard.querySelector('.etiqueta-desconto p');
 
             modalImagem.src = imagem.src;
             modalNome.textContent = nome;
-            modalPreco.textContent = preco;
 
+            // Verifica se o item possui preço de top e short
+            if (precoTop && precoShort) {
+                modalPreco.innerHTML = `${precoTop.textContent}<br>${precoShort.textContent}`;
+            } else {
+                const precoNormal = itemCard.querySelector('.item-price');
+                modalPreco.textContent = `Preço: ${precoNormal.textContent}`;
+            }
+
+            // Exibe o desconto se houver
             if (desconto) {
                 modalDesconto.style.display = 'block';
                 modalDesconto.textContent = desconto.textContent;
@@ -31,7 +40,7 @@ function maisInformacoes(){
             modalMaisInformacoes.style.display = 'flex';
             document.body.style.overflow = 'hidden';
 
-            function levarAoTopo(){
+            function levarAoTopo() {
                 window.scrollTo({
                     top: 0
                 });
